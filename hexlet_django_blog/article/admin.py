30b3600a89479163b7f66050e17d0779a9575112
Denwien <denisgrigoriev1973@gmail.com>
2026-01-07 17:08:42 +0100

    Добавлена регистрация модели Article в Django Admin с настройками поиска, фильтра и отображения

--- hexlet_django_blog/article/admin.py
+++ hexlet_django_blog/article/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.admin import DateFieldListFilter
+from .models import Article
 
-# Register your models here.
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    # Поля, которые будут отображаться в списке статей
+    list_display = ("name", "created_at")
+
+    # Поля, по которым будет работать поиск
+    search_fields = ["name", "body"]
+
+    # Фильтр по дате создания статьи
+    list_filter = (("created_at", DateFieldListFilter),)
