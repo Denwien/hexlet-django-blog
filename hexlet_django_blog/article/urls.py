ad87cafdc9fca15f7ab6d23c300223ce65cab7a3
Denwien <denisgrigoriev1973@gmail.com>
2026-01-08 17:32:51 +0100

    Добавлены изменения для редактирования и отображения статей

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from hexlet_django_blog.article.views import IndexView, ArticleView, ArticleFormCreateView
+from .views import IndexView, ArticleFormView, ArticleFormEditView, ArticleView
 
 urlpatterns = [
-    path("", IndexView.as_view(), name="articles"),
-    path("create/", ArticleFormCreateView.as_view(), name="articles_create"),
-    path("<int:id>/", ArticleView.as_view(), name="article"),
+    path("", IndexView.as_view(), name="index"),  # главная страница списка статей
+    path("create/", ArticleFormView.as_view(), name="articles_create"),  # создание статьи
+    path("<int:id>/edit/", ArticleFormEditView.as_view(), name="articles_update"),  # редактирование
+    path("<int:id>/", ArticleView.as_view(), name="article"),  # просмотр статьи
 ]
---
+++ hexlet_django_blog/article/views_update.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import Article
+from .forms import ArticleForm
+
+class ArticleFormEditView(View):
+    def get(self, request, *args, **kwargs):
+        article_id = kwargs.get("id")
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(instance=article)
+        return render(
+            request, "articles/update.html", {"form": form, "article_id": article_id}
+        )
+
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get("id")
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect("articles")
+        return render(
+            request, "articles/update.html", {"form": form, "article_id": article_id}
+        )
