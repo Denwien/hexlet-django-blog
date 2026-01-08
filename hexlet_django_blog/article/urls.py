679af8f83225e4b22af078af54cf20150e8a96cb
Denwien <denisgrigoriev1973@gmail.com>
2026-01-08 15:10:33 +0100

    Добавлены страницы списка статей и просмотра конкретной статьи

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
-from hexlet_django_blog.article.views import ArticleIndexView
+from hexlet_django_blog.article.views import IndexView, ArticleView
 
 urlpatterns = [
-    path("", ArticleIndexView.as_view(), name="articles"),
+    path("", IndexView.as_view(), name="articles_index"),
+    path("<int:id>/", ArticleView.as_view(), name="article"),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,16 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 
 from hexlet_django_blog.article.models import Article
 
-
-class ArticleIndexView(View):
+class IndexView(View):
     def get(self, request, *args, **kwargs):
         articles = Article.objects.all()[:15]
         return render(
             request,
             "articles/index.html",
-            {
-                "articles": articles,
-            },
+            context={"articles": articles},
+        )
+
+class ArticleView(View):
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, id=kwargs["id"])
+        return render(
+            request,
+            "articles/show.html",
+            context={"article": article},
         )
